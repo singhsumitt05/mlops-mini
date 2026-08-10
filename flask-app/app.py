ce2d819fbd3aed98f935437c16b365af7e4784c8
singhsumitt05 <singhsumitt05@gmail.com>
2026-08-10 11:37:48 +0530

    added token based auth to flask app

--- flask-app/app.py
+++ flask-app/app.py
@@ -4,11 +4,24 @@ import dagshub
 import pickle
 import pandas as pd
 import numpy as np
-
-dagshub.init(repo_owner='singhsumitt05', repo_name='mlops-mini', mlflow=True)
-mlflow.set_tracking_uri('https://dagshub.com/singhsumitt05/mlops-mini.mlflow')
+import os
 
 from preprocessing_utility import normalize_text
+
+# Set up DagsHub credentials for MLflow tracking
+dagshub_token = os.getenv("DAGSHUB_PAT")
+if not dagshub_token:
+    raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
+
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+dagshub_url = "https://dagshub.com"
+repo_owner = "singhsumitt05"
+repo_name = "mlops-mini"
+
+# Set up MLflow tracking URI
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 
 
 app = Flask(__name__)
